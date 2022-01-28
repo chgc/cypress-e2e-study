bd01fc2142f24e73e7fb33d2baf43d732d64b772
KevinYang <chgc.tw@gmail.com>
2022-01-28 23:11:40 +0800

    add todoMVC and finish add item e2e testing

--- cypress/integration/main.spec.ts
+++ cypress/integration/main.spec.ts
@@ -1,4 +1,19 @@
 describe('Main', () => {
-  it('', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Visits the initial project page', () => {
+    cy.contains('todos');
+  });
+
+  it('has two todo item in the list', () => {
+    cy.get('.todo-list').children().should('have.length', 0);
+  });
+
+  it('add new todo, list should have 3 items', () => {
+    // cy.get('.new-todo').type('abc').type('{enter}');
+    cy.get('[data-cy=newTodo]').type('abc').type('{enter}');
+    cy.get('.todo-list').children().should('have.length', 1);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
+type Todo = {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'e2e-study';
+  todos: Todo[] = [];
+
+  addTodo(ele: HTMLInputElement) {
+    this.todos.push({
+      id: uuidv4(),
+      content: ele.value,
+      isCompleted: false,
+    });
+    ele.value = '';
+  }
 }
